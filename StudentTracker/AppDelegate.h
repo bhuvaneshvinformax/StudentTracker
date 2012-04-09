f1802009cff24805905354f9204d3c47fb912ab3
Aron Bury <aron.bury@gmail.com>
2012-04-09 10:29:30 +1000

    Initial Commit

---
+++ StudentTracker/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  StudentTracker
+//
+//  Created by Aron Bury on 9/04/12.
+//  Copyright (c) 2012 Aron's IT Consultancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ StudentTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StudentTracker
+//
+//  Created by Aron Bury on 9/04/12.
+//  Copyright (c) 2012 Aron's IT Consultancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
